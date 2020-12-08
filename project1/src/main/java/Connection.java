54ab112825d6b85dac2b8f0fc4b831080b653f5e
sli10 <43591151+sli10@users.noreply.github.com>
2020-12-08 16:53:53 -0600

    Add files via upload

---
+++ project1/src/main/java/Connection.java
@@ -0,0 +1,38 @@
+import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+public class Connection {
+    public static void main(String[] args) {
+        String MySQLURL = "jdbc:mysql://localhost:3306/salesSmall?useSSL=false";
+        String databseUserName = "root";
+        String databasePassword = "Pok3mon4evr!";
+        Connection con = null;
+//        Statement stmt = con.createStatement();
+        ResultSet resultSet = null;
+        String query = "";
+
+        try {
+
+            // open connection
+            con = DriverManager.getConnection(MySQLURL, databseUserName, databasePassword);
+            if (con != null) {
+            	 Statement stmt = con.createStatement();
+//                System.out.println("Database connection is successful !!!!");
+                query = "SELECT P_CODE FROM salesSmall.PRODUCT";
+                resultSet = stmt.executeQuery(query);
+                
+                while (resultSet.next()) {
+                    System.out.println(resultSet.getString("P_CODE"));
+
+                }
+
+
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
