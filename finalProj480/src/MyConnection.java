b505ebfab286bc93875e655261d8c3a39de97a61
sli10 <sli235@uic.edu>
2020-12-09 16:38:24 -0600

    added txt for login

--- finalProj480/src/MyConnection.java
+++ finalProj480/src/MyConnection.java
@@ -1,10 +1,20 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 import java.sql.*;
 import java.sql.DriverManager;
+import java.util.Scanner;
 
 public class MyConnection {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         String MySQLURL = "jdbc:mysql://localhost:3306/salesSmall?useSSL=false";
-        String databseUserName = "root";
+        File file = new File("./login.txt");
+        Scanner sc = new Scanner(file);
+        sc.useDelimiter(sc.next());
+
+        System.out.println();
+
+        String databaseUserName = "root";
         String databasePassword = "Pok3mon4evr!";
         Connection con = null;
 //        Statement stmt = con.createStatement();
@@ -14,7 +24,7 @@ public class MyConnection {
         try {
 
             // open connection
-            con = DriverManager.getConnection(MySQLURL, databseUserName, databasePassword);
+            con = DriverManager.getConnection(MySQLURL, databaseUserName, databasePassword);
             if (con != null) {
             	 Statement stmt = con.createStatement();
 //                System.out.println("Database connection is successful !!!!");
