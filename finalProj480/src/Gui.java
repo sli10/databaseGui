731462f0996833cbd5fae20f45741d1a5d7f4da1
Judy Park <parkjudy57@gmail.com>
2020-12-10 01:31:08 -0600

    Gui

--- finalProj480/src/Gui.java
+++ finalProj480/src/Gui.java
@@ -24,10 +24,11 @@ import javax.swing.*;
 
 public class Gui extends Application {
     HashMap<String, Scene> sceneMap;
-    Button start, logIn, findShows;
+    Button start, logIn, findShows, cont;
     ComboBox<String> genresList, ratingsRange, platformsList, ageRanges;
     TextField userName, password;
     String holdLogInStmt, chosenGenre, chosenAge, chosenPlatform, chosenRating;
+    Text title, description, genre, IMBD, rottenScore, age, released, seasons, stream;
     Text logInErrorStmt;
     MyConnection connect = new MyConnection();
     HashMap<String, String> userPassList = new HashMap<String, String>();
@@ -49,16 +50,20 @@ public class Gui extends Application {
         primaryStage.show();
 
         //buttons: logIn, start, findShows
-        start = new Button("Start Looking");
-        logIn = new Button("Log In");
-        findShows = new Button("Find Shows");
+        start = new Button("START LOOKING");
+        start.setStyle("-fx-background-color: SNOW;");
+        logIn = new Button("LOG IN");
+        logIn.setStyle("-fx-background-color: SNOW;");
+        findShows = new Button("FIND SHOWS");
+        findShows.setStyle("-fx-background-color: SNOW;");
+        cont = new Button("CONTINUE");
+        cont.setStyle("-fx-background-color: SNOW;");
 
         //change scene at start
         start.setOnAction(e -> {
                     primaryStage.setScene(sceneMap.get("logIn"));
                     try {
                         connect.connect();
-                        connect.getPlatform("Netflix");
                     } catch (FileNotFoundException fileNotFoundException) {
                         fileNotFoundException.printStackTrace();
                     }
@@ -102,6 +107,36 @@ public class Gui extends Application {
 
         findShows.setOnAction(e-> {
             primaryStage.setScene(sceneMap.get("recommendations"));
+            System.out.println("chosen platform: " + chosenPlatform);
+            System.out.println("chosen genre: " + chosenGenre);
+            System.out.println("chosen rating: " + chosenRating);
+            System.out.println("chosen age: " + chosenAge);
+            connect.getPlatform(chosenPlatform);
+            connect.getGenre(chosenGenre);
+            connect.getRange(chosenRating);
+            connect.getContent(chosenAge);
+            connect.runQuery();
+            connect.pickOne();
+            updateText(connect.getTitle(), connect.getReleased(), connect.getAge(), connect.getIMBD(), connect.getRotten(),
+                    connect.getGenre(), connect.getDescription(), connect.getSeasons(), connect.getStream());
+            connect.clear();
+        });
+
+        cont.setOnAction(e-> {
+            genresList.getSelectionModel().clearSelection();
+            ratingsRange.getSelectionModel().clearSelection();
+            platformsList.getSelectionModel().clearSelection();
+            ageRanges.getSelectionModel().clearSelection();
+            title.setText("");
+            description.setText("");
+            genre.setText("");
+            IMBD.setText("");
+            rottenScore.setText("");
+            age.setText("");
+            released.setText("");
+            seasons.setText("");
+            stream.setText("");
+            primaryStage.setScene(sceneMap.get("selections"));
         });
 
         sceneMap.put("start", startScene());
@@ -118,22 +153,26 @@ public class Gui extends Application {
         BorderPane pane = new BorderPane();
 
         //welcome text
-        Text t = new Text(500, 100, "Are You \n Looking For TV Shows\n to Watch?");
-        t.setFont(Font.font("Monospace", FontWeight.BOLD, 70));
+        Text t = new Text("LOOKING FOR A SHOW?");
+        t.setFont(Font.font("Helvetica", FontWeight.BOLD, 30));
         t.setTextAlignment(TextAlignment.CENTER);
-        t.setFill(Color.web("#ad8989"));
-        t.setStroke(Color.web("#401921"));
-        t.setStrokeWidth(2);
+        t.setFill(Color.BURLYWOOD);
+
+        Text t2 = new Text("YOU CAME TO THE RIGHT PLACE");
+        t2.setFont(Font.font("Helvetica", FontWeight.BOLD, 20));
+        t2.setTextAlignment(TextAlignment.CENTER);
+        t2.setFill(Color.BURLYWOOD);
+
 
         //organize with vbox
-        VBox paneCenter = new VBox(10, t, start);
+        VBox paneCenter = new VBox(10, t, t2, start);
         paneCenter.setPadding(new Insets(100));
         paneCenter.setAlignment(Pos.CENTER);
+        paneCenter.setStyle("-fx-background-color: azure;");
         addToGenre();
         pane.setCenter(paneCenter);
-//        pane.setStyle(Color.web("#ffffff"));
 
-        return new Scene(pane, 1200, 800);
+        return new Scene(pane, 700, 700);
     }
 
     //log into your account to get the list of shows already watched
@@ -141,14 +180,14 @@ public class Gui extends Application {
         BorderPane pane = new BorderPane();
 
         //username text before text field
-        Text user = new Text(550, 20, "UserName: \n");
-        user.setFont(Font.font("Monospace", FontWeight.BOLD, 20));
-        user.setFill(Color.web("#401921"));
+        Text user = new Text("UserName: \n");
+        user.setFont(Font.font("Helvetica", FontWeight.BOLD, 20));
+        user.setFill(Color.SEAGREEN);
 
         //password text before text field
-        Text pass = new Text(550, 20, "Password: \n");
-        pass.setFont(Font.font("Monospace", FontWeight.BOLD, 20));
-        pass.setFill(Color.web("#401921"));
+        Text pass = new Text("Password: \n");
+        pass.setFont(Font.font("Helvetica", FontWeight.BOLD, 20));
+        pass.setFill(Color.SEAGREEN);
 
         //initialize text fields
         userName = new TextField();
@@ -166,8 +205,8 @@ public class Gui extends Application {
         holdPass.setAlignment(Pos.CENTER);
 
         logInErrorStmt = new Text();
-        logInErrorStmt.setFont(Font.font("Monospace", FontWeight.SEMI_BOLD, 15));
-        logInErrorStmt.setFill(Color.web("#401921"));
+        logInErrorStmt.setFont(Font.font("Helvetica", FontWeight.SEMI_BOLD, 15));
+        logInErrorStmt.setFill(Color.MINTCREAM);
 
         //vBox - vertical box to keep user and pass
         VBox paneCenter = new VBox(10, holdUser, holdPass, logIn, logInErrorStmt);
@@ -175,8 +214,9 @@ public class Gui extends Application {
         paneCenter.setAlignment(Pos.CENTER);
 
         pane.setCenter(paneCenter);
+        pane.setStyle("-fx-background-color: thistle;");
 
-        return new Scene(pane, 1200, 800);
+        return new Scene(pane, 700, 700);
     }
 
     public void addToGenre(){
@@ -201,6 +241,31 @@ public class Gui extends Application {
         ageRanges.getItems().addAll("18", "16", "7");
     }
 
+    public void updateText(String titleStr, String releasedStr, String ageStr, String IMBDStr, String rottenStr,
+                           String genreStr, String descStr, String seasonStr, String streamStr) {
+        System.out.println("titleStr: " + titleStr);
+
+        if (titleStr != null ) {
+            title.setText("Title: " + titleStr);
+            released.setText("Released Year: " + releasedStr);
+            age.setText("Content Rating: " + ageStr);
+            IMBD.setText("IMBD Rating: " + IMBDStr);
+            rottenScore.setText("Rotten Score: " + rottenStr);
+            genre.setText("Genre: " + genreStr);
+            description.setText("Description: " + descStr);
+            String temp = seasonStr;
+            String delims = "S";
+            String[] tokens = temp.split(delims);
+
+            String seasonNum = tokens[0];
+            seasons.setText("No. of Seasons: " + seasonNum + " Season(s)");
+            stream.setText("Streaming Platform: " + streamStr);
+        }
+        else {
+            title.setText("No TV Shows Match... Please Try Again!" );
+        }
+    }
+    
     //scene to keep record of the options
     public Scene optionsScene() {
         BorderPane pane = new BorderPane();
@@ -210,26 +275,26 @@ public class Gui extends Application {
         paneCenter.setAlignment(Pos.CENTER);
 
         //genre - text and declare comboBox
-        Text t2 = new Text(550, 20, "What genre are you interested in? \n");
-        t2.setFont(Font.font("Monospace", FontWeight.BOLD, 20));
+        Text t2 = new Text(550, 20, "WHAT GENRE ARE YOU INTERESTED IN? \n");
+        t2.setFont(Font.font("Helvetica", FontWeight.BOLD, 20));
         t2.setFill(Color.web("#401921"));
         addToGenre();
 
         //ratings - text and declare comboBox
-        Text t3 = new Text(550, 20, "What range of ratings? \n");
-        t3.setFont(Font.font("Monospace", FontWeight.BOLD, 20));
+        Text t3 = new Text(550, 20, "WHAT RANGE SHOULD THE RATING BE? \n");
+        t3.setFont(Font.font("Helvetica", FontWeight.BOLD, 20));
         t3.setFill(Color.web("#401921"));
         addToRatings();
 
         //platform - text and declare comboBox
-        Text t4 = new Text(550, 20, "What platform do you watch on? \n");
-        t4.setFont(Font.font("Monospace", FontWeight.BOLD, 20));
+        Text t4 = new Text(550, 20, "WHAT PLATFORM DO YOU WANT TO WATCH ON? \n");
+        t4.setFont(Font.font("Helvetica", FontWeight.BOLD, 20));
         t4.setFill(Color.web("#401921"));
         addToPlatforms();
 
         //Age - text and declare comboBox
-        Text t5 = new Text(550, 20, "Content Ratings? \n");
-        t5.setFont(Font.font("Monospace", FontWeight.BOLD, 20));
+        Text t5 = new Text(550, 20, "WHAT SHOULD BE THE CONTENT RATING? \n");
+        t5.setFont(Font.font("Helvetica", FontWeight.BOLD, 20));
         t5.setFill(Color.web("#401921"));
         addToAge();
 
@@ -264,10 +329,11 @@ public class Gui extends Application {
 
         //all items to vbox and reset pane
         paneCenter.getChildren().addAll(t4, platformsList, t2, genresList, t3, ratingsRange, t5, ageRanges, findShows);
-        pane.setTop(paneCenter);
         pane.setStyle("-fx-background-color:#ad8989");
 
-        return new Scene(pane, 1000, 800);
+        pane.setCenter(paneCenter);
+
+        return new Scene(pane, 700, 700);
 
     }
 
@@ -276,16 +342,53 @@ public class Gui extends Application {
         BorderPane pane = new BorderPane();
 
         //header text
-        Text rec = new Text(550, 20, "Here Are Your Recommendations: \n");
-        rec.setFont(Font.font("Monospace", FontWeight.BOLD, 20));
-        rec.setFill(Color.web("#401921"));
+        Text rec = new Text(550, 20, "HERE IS YOUR RECOMMENDATION: \n");
+        rec.setFont(Font.font("Helvetica", FontWeight.BOLD, 30));
+        rec.setFill(Color.FLORALWHITE);
+
+        title = new Text();
+        title.setFont(Font.font("Helvetica", FontWeight.BOLD, 20));
+        title.setFill(Color.OLDLACE);
+
+        released = new Text();
+        released.setFont(Font.font("Helvetica", FontWeight.BOLD, 14));
+        released.setFill(Color.PAPAYAWHIP);
+
+        age = new Text();
+        age.setFont(Font.font("Helvetica", FontWeight.BOLD, 14));
+        age.setFill(Color.PAPAYAWHIP);
+
+        IMBD = new Text();
+        IMBD.setFont(Font.font("Helvetica", FontWeight.BOLD, 14));
+        IMBD.setFill(Color.PAPAYAWHIP);
+
+        rottenScore = new Text();
+        rottenScore.setFont(Font.font("Helvetica", FontWeight.BOLD, 14));
+        rottenScore.setFill(Color.PAPAYAWHIP);
+
+        genre = new Text();
+        genre.setFont(Font.font("Helvetica", FontWeight.BOLD, 14));
+        genre.setFill(Color.PAPAYAWHIP);
+
+        description = new Text();
+        description.setFont(Font.font("Helvetica", FontWeight.BOLD, 14));
+        description.setWrappingWidth(600);
+        description.setFill(Color.PAPAYAWHIP);
+
+        seasons = new Text();
+        seasons.setFont(Font.font("Helvetica", FontWeight.BOLD, 14));
+        seasons.setFill(Color.PAPAYAWHIP);
+
+        stream = new Text();
+        stream.setFont(Font.font("Helvetica", FontWeight.BOLD, 14));
+        stream.setFill(Color.PAPAYAWHIP);
 
         //vbox to organize
-        VBox paneCenter = new VBox(10, rec);
-        pane.setTop(paneCenter);
-        pane.setStyle("-fx-background-color:#ad8989");
-
-        return new Scene(pane, 1000, 800);
+        VBox paneCenter = new VBox(10, rec, title, released, age, rottenScore, genre, description, seasons, stream, cont);
+        paneCenter.setAlignment(Pos.CENTER);
+        pane.setCenter(paneCenter);
+        pane.setStyle("-fx-background-color: LIGHTSTEELBLUE;");
+        return new Scene(pane, 700, 700);
     }
 
 }
